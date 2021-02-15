4dd755e14aaaef084cb0c200d16e9d6a27a9c76d
u-uysal <ufuk.uysl5@gmail.com>
2021-02-15 22:52:02 +0100

    event handli

--- Vue-3/main.js
+++ Vue-3/main.js
@@ -1,17 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart:0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
-            // solution
-            sizes: ['S', 'M', 'L', 'XL'],
-            // solution
             variants: [
-              { id: 2234, color: 'green' },
-              { id: 2235, color: 'blue' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
+        }
+    },
+    methods : {
+        addCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            this.cart -=1
         }
     }
 })
